cb6ca67e80098afe5a186e961d3bbdb63dee06c7
Gaur, Piyush (CW) <piyush.gaur@polycom.com>
2019-05-01 18:59:47 +0530

    testing h323

--- src/messaging-extension.js
+++ src/messaging-extension.js
@@ -5,14 +5,15 @@ module.exports.setup = function () {
     var teamsBuilder = require('botbuilder-teams');
     var bot = require('./bot');
 
-    bot.connector.onQuery('joinMeeting', function(event, query, callback) {
+    bot.connector.onQuery('joinMeeting', function (event, query, callback) {
         //var faker = require('faker');
 
         // If the user supplied a title via the cardTitle parameter then use it or use a fake title
         var title = query.parameters && query.parameters[0].name === 'meetingId'
             ? query.parameters[0].value
-            : parseInt(Math.random()*10000);
+            : parseInt(Math.random() * 10000);
 
+        console.log('title: ', title);
         // let randomImageUrl = "https://loremflickr.com/200/200"; // Faker's random images uses lorempixel.com, which has been down a lot
 
         // Build the data to send
@@ -20,19 +21,19 @@ module.exports.setup = function () {
 
         // Generate 5 results to send with fake text and fake images
         //for (var i = 0; i < 5; i++) {
-            attachments.push(
-                new builder.ThumbnailCard()
-                    .title('Click to join meeting')
-                    .buttons([
-                        {
-                            type: "openUrl",
-                            title: "Join Meeting",
-                            value: `https://news.google.com`
-                        }
-                    ])
-                    //.text(faker.lorem.paragraph())
-                    //.images([new builder.CardImage().url(`${randomImageUrl}?random=${i}`)])
-                    .toAttachment());
+        attachments.push(
+            new builder.ThumbnailCard()
+                .title('Click to join meeting')
+                .buttons([
+                    {
+                        type: "openUrl",
+                        title: "Join Meeting",
+                        value: `h323://${title}`
+                    }
+                ])
+                //.text(faker.lorem.paragraph())
+                //.images([new builder.CardImage().url(`${randomImageUrl}?random=${i}`)])
+                .toAttachment());
         //}
 
         // Build the response to be sent
@@ -58,7 +59,7 @@ module.exports.setup = function () {
     //     var attachments = [];
 
     //     const caList = [];
-        
+
     //     caList.push(new builder.CardAction()
     //         .title('Join').type('openUrl').value(`h323://${title}`)
     //     );
