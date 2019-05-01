dbc40b35be7ba508e014cf899a69623a50a23ee5
Gaur, Piyush (CW) <piyush.gaur@polycom.com>
2019-05-01 13:41:49 +0530

    customized for poly bot

--- src/messaging-extension.js
+++ src/messaging-extension.js
@@ -5,31 +5,56 @@ module.exports.setup = function() {
     var teamsBuilder = require('botbuilder-teams');
     var bot = require('./bot');
 
-    bot.connector.onQuery('getRandomText', function(event, query, callback) {
-        var faker = require('faker');
+    // bot.connector.onQuery('getRandomText', function(event, query, callback) {
+    //     var faker = require('faker');
+
+    //     // If the user supplied a title via the cardTitle parameter then use it or use a fake title
+    //     var title = query.parameters && query.parameters[0].name === 'cardTitle'
+    //         ? query.parameters[0].value
+    //         : faker.lorem.sentence();
+
+    //     let randomImageUrl = "https://loremflickr.com/200/200"; // Faker's random images uses lorempixel.com, which has been down a lot
+
+    //     // Build the data to send
+    //     var attachments = [];
+
+    //     // Generate 5 results to send with fake text and fake images
+    //     for (var i = 0; i < 5; i++) {
+    //         attachments.push(
+    //             new builder.ThumbnailCard()
+    //                 .title(title)
+    //                 .text(faker.lorem.paragraph())
+    //                 .images([new builder.CardImage().url(`${randomImageUrl}?random=${i}`)])
+    //                 .toAttachment());
+    //     }
+
+    //     // Build the response to be sent
+    //     var response = teamsBuilder.ComposeExtensionResponse
+    //         .result('list')
+    //         .attachments(attachments)
+    //         .toResponse();
+
+    //     // Send the response to teams
+    //     callback(null, response, 200);
+    // });
+
+    bot.connector.onQuery('joinMeeting', function(event, query, callback) {
+        
 
         // If the user supplied a title via the cardTitle parameter then use it or use a fake title
-        var title = query.parameters && query.parameters[0].name === 'cardTitle'
+        var title = query.parameters && query.parameters[0].name === 'meetingId'
             ? query.parameters[0].value
-            : faker.lorem.sentence();
-
-        let randomImageUrl = "https://loremflickr.com/200/200"; // Faker's random images uses lorempixel.com, which has been down a lot
+            : '12345';
 
         // Build the data to send
         var attachments = [];
 
-        // Generate 5 results to send with fake text and fake images
-        for (var i = 0; i < 5; i++) {
-            attachments.push(
-                new builder.ThumbnailCard()
-                    .title(title)
-                    .text(faker.lorem.paragraph())
-                    .images([new builder.CardImage().url(`${randomImageUrl}?random=${i}`)])
-                    .toAttachment());
-        }
+        let ca = new builder.CardAction().title('Join').type('openUrl').value(`h323://${title}`);
+
+        attachments.push(ca);
 
         // Build the response to be sent
-        var response = teamsBuilder.ComposeExtensionResponse
+        var response = teamsBuilder.ComposeExtensionResponse()
             .result('list')
             .attachments(attachments)
             .toResponse();
